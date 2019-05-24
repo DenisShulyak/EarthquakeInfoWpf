8f95581fbc63569a835f1a10ed4a2ad1a3604639
Denis <denshufly@mail.ru>
2019-05-24 18:19:08 +0600

    EarthquakeInfo

---
+++ InfoEarthquakeWpf/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using InfoEarthquakeWpf.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+
+namespace InfoEarthquakeWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<Earthquake> _data;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            System.Net.WebClient client = new System.Net.WebClient();
+            Stream stream = client.OpenRead("https://earthquake-report.com/feeds/recent-eq?json");
+            StreamReader sr = new StreamReader(stream);
+
+
+            string txt = sr.ReadLine();
+
+            List<Earthquake> data = JsonConvert.DeserializeObject<List<Earthquake>>(txt);
+           
+
+            _data = data;
+            itemsDataGrid.ItemsSource = new List<Earthquake>(_data);
+           
+        }
+
+        private void ShowButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+
+            int countRecords = int.Parse(countRecordsTextBox.Text);
+
+
+            _data.RemoveRange(countRecords,(_data.Count - countRecords));
+            
+
+            itemsDataGrid.ItemsSource = null;
+            itemsDataGrid.ItemsSource = new List<Earthquake>(_data);
+            
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void InfoButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                int index = itemsDataGrid.SelectedIndex;
+
+                MessageBox.Show("Magnitude: " + _data[index].Magnitude + "\n"
+                    + "Place: " + _data[index].Location + " Latitude: " + _data[index].Latitude + " Longitude: " + _data[index].Longitude + "\n"
+                    + "Date and time: " + _data[index].DateTime.ToLongDateString() + "\n"
+                    + "Depth: " + _data[index].Depth);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ InfoEarthquakeWpf/Models/RootObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InfoEarthquakeWpf.Models
+{
+    public class Earthquake 
+    {
+        public string Title { get; set; }
+        public string Magnitude { get; set; }
+        public string Location { get; set; }
+        public string Depth { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Link { get; set; }
+
+
+    }
+}
